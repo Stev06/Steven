ba2b6bd03a4eabee504835ac822c1d65f122d02a
Stev06 <62486236+Stev06@users.noreply.github.com>
2024-05-17 13:16:56 -0400

    Add files via upload

--- ICS CULMINATING/js/sketchPad.js
+++ ICS CULMINATING/js/sketchPad.js
@@ -1,5 +1,5 @@
 class SketchPad{
-    constructor(Container,size=400){
+    constructor(container,size=400){
         this.canvas=document.createElement("canvas");
         this.canvas.width=size;
         this.canvas.height=size;
@@ -7,12 +7,20 @@ class SketchPad{
             background-color:white;
             box-shadow: 0px 0px 10px 2px black;
         `;
-        Container.appendChild(this.canvas);
+        container.appendChild(this.canvas);
+
+        const lineBreak=document.createElement("br");
+        container.appendChild(lineBreak);
+
+        this.undoBtn=document.createElement("button");
+        this.undoBtn.innerHTML="UNDO";
+        container.appendChild(this.undoBtn);
 
         this.ctx=this.canvas.getContext("2d");
 
         this.paths=[];
         this.isDrawing = false;
+        this.#redraw();
 
         this.#addEventListeners();
     }
@@ -35,13 +43,35 @@ class SketchPad{
         this.canvas.onmouseup=()=>{
             this.isDrawing=false;
         }
-        
+        this.canvas.ontouchstart=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousedown(loc);
+        }
+        this.canvas.ontouchmove=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmousemove(loc);
+        }
+        this.canvas.ontouchend=(evt)=>{
+            const loc=evt.touches[0];
+            this.canvas.onmouseup(loc);
+        }
+        this.undoBtn.onclick=()=>{
+            this.paths.pop();
+            this.#redraw();
+        }
     }
 
     #redraw(){
         this.ctx.clearRect(0,0,
             this.canvas.width, this.canvas.height);
         draw.paths(this.ctx,this.paths);
+
+        if(this.paths.length>0){
+            this.undoBtn.disabled=false;
+        }
+        else{
+            this.undoBtn.disabled=true;
+        }
     }
 
     #getMouse=(evt)=>{
